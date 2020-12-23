b5ead188296a499a7e460d6f7df6dd7f57d4d169
raychenchen <raychenchen@tencent.com>
2020-12-23 14:52:51 +0800

    add get ip

--- help/url.go
+++ help/url.go
@@ -1,7 +1,26 @@
 package help
 
-import "net/url"
+import (
+	"net"
+	"net/url"
+)
 
 func UrlEncode(str string) string {
 	return url.QueryEscape(str)
+}
+
+func GetIpAddr() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
 }
