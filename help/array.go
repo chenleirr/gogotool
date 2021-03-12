c071800b23cb100eef7f2abd6ccbf85b5abb54b8
raychenchen <raychenchen@tencent.com>
2021-03-12 14:09:10 +0800

    add ArrayChunk

--- help/array.go
+++ help/array.go
@@ -1,5 +1,6 @@
 package help
 
+// 去除slice中重复元素和空字符串
 func ArrayUnique(ids []string) []string {
 	m := make(map[string]bool)
 	for _, id := range ids {
@@ -12,5 +13,27 @@ func ArrayUnique(ids []string) []string {
 	for k := range m {
 		result = append(result, k)
 	}
+	return result
+}
+
+// SliceChunk 切分slice为相同size的slice
+func ArrayChunk(data []string, chunkSize int) [][]string {
+	var result [][]string
+	if chunkSize <= 0 {
+		return result
+	}
+
+	data = ArrayUnique(data)
+
+	for i := 0; i < len(data); i += chunkSize {
+		end := i + chunkSize
+
+		if end > len(data) {
+			end = len(data)
+		}
+
+		result = append(result, data[i:end])
+	}
+
 	return result
 }
